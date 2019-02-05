da6fef4b99947049548e4d18e473b93dc19c5df2
Vinamra Singh <adwivedi10@sapient.com>
2019-02-05 16:55:20 +0530

    Action reducers complete

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -4,8 +4,37 @@ import { bindActionCreators } from "redux";
 //1. action (info about what happened)
 //2. copy of current state
 
-function comments(state=[],action){
+function postComments(state=[],action){
     console.log(state,action);
+    switch(action.type){
+        case 'ADD_COMMENT':
+        return [...state,{
+            user:action.author,
+            text:action.comment
+        }];
+        case 'REMOVE_COMMENT':
+        console.log('Removing a comment');
+        return [
+            ...state.slice(0,action.i),
+            ...state.slice(action.i +1 )
+        ];
+        case 'default':
+        return state;
+    }
+    return state;
+}
+
+function comments(state=[],action){
+    if(typeof action.postId !== 'undefined'){
+        return{
+            //take the current state
+            ...state,
+            //overwrite this post with a new one
+            [action.postId]:postComments(state[action.postId],action)
+
+        }
+
+    }
     return state;
 }
 
